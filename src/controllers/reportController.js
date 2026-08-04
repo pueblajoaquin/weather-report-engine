dba82104cce599004ff5b0fc9abff5dc8252e512
joako-code <pueblajakoo@gmail.com>
2026-08-03 23:50:45 -0300

    change name of function in createReportcontrollr

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,6 +1,6 @@
 import { createReportService } from '../services/reportService.js'
 
-export async function createReport(req, res) {
+export async function createReportController(req, res) {
     const { city, startDate, endDate } = req.body
 
     if (!city || !startDate || !endDate) {

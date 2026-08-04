3fdc3dae9f3f67467d3e05e47db93211faa98d6c
joako-code <pueblajakoo@gmail.com>
2026-08-04 19:28:53 -0300

    add new controller functions

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,4 +1,5 @@
-import { createReportService } from '../services/reportService.js'
+import fs, { fsyncSync } from 'node:fs'
+import { createReportService, getReportByIdService, listReportsService, retryReportService } from '../services/reportService.js'
 
 export async function createReportController(req, res) {
     const { city, startDate, endDate } = req.body
@@ -13,5 +14,44 @@ export async function createReportController(req, res) {
     } catch (error) {
         console.error(error)
         return res.status(500).json({ error: 'Interval server error' })
+    }
+}
+
+export async function getReportStatusController(req, res) {
+    const { id } = req.params
+
+    try {
+        const report = await getReportByIdService(id)
+
+        if (!report) {
+            return res.status(404).json({ error: 'Report not found' })
+        }
+
+        return res.status(200).json(report)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ error: 'Internal server error' })
+    }
+}
+
+export async function downloadReportService(req, res) {
+    const { id } = req.params
+
+    try {
+        const report = await getReportByIdService(id)
+
+        if (!report) {
+            return res.status(404).json({ error: 'Report not found' })
+        }
+
+        if (report.status !== 'completed') {
+            return res.status(409).json({ error: 'Report is not ready for download' })
+        }
+
+        if (!report.filePath || !fs.exists(report.filePath)) {
+            return res.status(404).json({ error: 'Report file not found on server' })
+        }
+    } catch (error) {
+
     }
 }

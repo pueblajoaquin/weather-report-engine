b4e7327124c466e9544f36a2dcf501ffeb2421fe
joako-code <pueblajakoo@gmail.com>
2026-08-04 19:35:49 -0300

    add get / reports, get / reports/:id, get / reports/ :id/ download

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express'
-import { createReportController } from '../controllers/reportController.js'
+import { createReportController, getAllReportsController, downloadReportController, getReportStatusController } from '../controllers/reportController.js'
 
 const router = Router()
 
 router.post('/reports', createReportController)
+
+router.get('/reports', getAllReportsController)
+
+router.get('/reports/:id', getReportStatusController)
+
+router.get('/reports/:id/download', downloadReportController)
 
 export default router
